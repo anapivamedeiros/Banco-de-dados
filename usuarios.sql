be74bb1da399376bba0795538361de9713385306
anapivamedeiros <ana.piva1a@gmail.com>
2026-03-05 15:43:35 -0300

    Usuarios- cadastro

--- usuarios.sql
+++ usuarios.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 03/03/2026 às 19:20
+-- Tempo de geração: 05/03/2026 às 19:43
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -38,15 +38,20 @@ CREATE TABLE `usuarios` (
   `complemento` varchar(80) DEFAULT NULL,
   `cep` varchar(10) DEFAULT NULL,
   `cidade` varchar(100) DEFAULT NULL,
-  `estado` varchar(50) DEFAULT NULL
+  `estado` varchar(50) DEFAULT NULL,
+  `foto` longtext DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `perfil`, `endereco`, `bairro`, `complemento`, `cep`, `cidade`, `estado`) VALUES
-(1, 'Maria da Silva', 'maria.silva@email.com', '123456', 'ADMIN', 'Rua das Flores, 123', 'Centro', 'Apto 202', '12345-678', 'São Paulo', 'SP');
+INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `perfil`, `endereco`, `bairro`, `complemento`, `cep`, `cidade`, `estado`, `foto`) VALUES
+(40, 'Marcela da Silva', 'marcela.silva@email.com', '123456', 'ADMIN', 'Rua das Flores, 123', 'Centro', 'Apto 202', '12345-678', 'São Paulo', 'SP', NULL),
+(45, 'Isabela Souza', 'isa.souza@email.com', '321654', 'ADMIN', 'Rua da paz, 356', 'Centro', 'Apto 891', '85234-761', 'São Paulo', 'SP', NULL),
+(48, 'Amanda Oliveira', 'manda.oliv@gmail.com', '1234589', 'ADMIN', 'Rua do Amor, 741', 'Centro', 'Apto 162', '78513-943', 'São Paulo', 'SP', NULL),
+(49, 'Milena Maia', 'milena.maia@gmail.com', '842397', 'USER', 'Rua da Paixão, 824', 'Centro', 'Apto 172', '78941-648', 'São Paulo', 'SP', NULL),
+(51, 'Larissa Ros carvalho Silva', 'lari.ros@email.com', '$2a$10$P9usXx7pFb6GgX6J/q20nOaEhTt8iZijr0.N/yaquP6Z.kKjCATbO', 'USER', 'Rua das Margaridas, 123', 'Centro', 'Apto 423', '78945-197', 'São Paulo', 'SP', NULL);
 
 --
 -- Índices para tabelas despejadas
@@ -67,7 +72,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
